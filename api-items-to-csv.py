597b53e7edb541ea84ea3bb169dcf2a7e6eed878
stephen <rowans55@yahoo.co.uk>
2017-10-18 19:26:01 +0100

    End of work commit

--- api-items-to-csv.py
+++ api-items-to-csv.py
@@ -27,19 +27,18 @@ csv_out = open('api-items.csv', mode='w') #opens csv file
 
 writer = csv.writer(csv_out) #create the csv writer object
 
-fields = ['GATE Reference code','GATE Title','GATE Name of Creator','GATE Dates of Creation'] #field names
-writer.writerow(fields) #writes field
+#field names
+
+fields = ['GATE Reference code','GATE Title','GATE Name of Creator','GATE Dates of Creation', 'GATE Level of description', 'GATE Collection', 'GATE Extent and medium of the unit of description', 'GATE Physical characteristics and technical requirements', 'GATE Conditions governing access'] 
+
+#writes fields
+
+writer.writerow(fields)
+
+
 
 #loop through json data :
 
-
- 
-    #writes a row and gets the fields from the json object
-
-
-#    print line["element_texts"][0]["element"]["name"], ":", line["element_texts"][0]["text"]
-
-#    print line["element_texts"][1]["element"]["name"], ":", line["element_texts"][1]["text"]
 
 for line in data_python:
         
@@ -49,43 +48,46 @@ for line in data_python:
         print line["element_texts"][i]["element"]["name"], ":", line["element_texts"][i]["text"]
 
 
-
         if (line["element_texts"][i]["element"]["name"]).endswith('code'):
                     GATE_Reference_Code = (line["element_texts"][i]["text"])
-
-
+        #else: GATE_Reference_Code = 0
+        
         if (line["element_texts"][i]["element"]["name"]).endswith('Title'):
                     GATE_Title = (line["element_texts"][i]["text"])
+        #else: GATE_Title = 0
                     
-                    writer.writerow([GATE_Reference_Code, GATE_Title])
+        if (line["element_texts"][i]["element"]["name"]).endswith('Creator'):
+                   GATE_Name_of_Creator = (line["element_texts"][i]["text"])
+        #else: GATE_Name_of_Creator = 0
+        
+        if (line["element_texts"][i]["element"]["name"]).endswith('Creation'):
+                   GATE_Dates_of_Creation = (line["element_texts"][i]["text"])
+        #else: GATE_Dates_of_Creation = 0
+                   
+        if (line["element_texts"][i]["element"]["name"]).endswith('Level of description'):
+                   GATE_Level_of_description = (line["element_texts"][i]["text"])
+        #else: GATE_Level_of_description = 0
+
+        GATE_Collection = (line["collection"]["id"])
+
+        if (line["element_texts"][i]["element"]["name"]).endswith('unit of description'):
+                   GATE_Extent_and_medium_of_the_unit_of_description = (line["element_texts"][i]["text"])
+        #else: GATE_Extent_and_medium_of_the_unit_of_description = 0                   
+
+        if (line["element_texts"][i]["element"]["name"]).endswith('requirements'):
+                    GATE_Physical_characteristics_and_technical_requirements = (line["element_texts"][i]["text"])
+        else: GATE_Physical_characteristics_and_technical_requirements = 0
+
+        if (line["element_texts"][i]["element"]["name"]).endswith('access'):
+                   GATE_Conditions_governing_access = (line["element_texts"][i]["text"])
+        #else: GATE_Conditions_governing_access = 0
+
+
+
+                 
+
+    writer.writerow([GATE_Reference_Code, GATE_Title, GATE_Name_of_Creator, GATE_Dates_of_Creation, GATE_Level_of_description, GATE_Collection, GATE_Extent_and_medium_of_the_unit_of_description, GATE_Physical_characteristics_and_technical_requirements, GATE_Conditions_governing_access])
 
                    
-
-
-
-                    
-#        GATE_Reference_Code = (line["element_texts"][0]["text"])
- #       GATE_Title = (line["element_texts"][0]["text"])
-  #      GATE_Name_of_Creator = (line["element_texts"][0]["text"])
-        
-
-
-
-        
-
-#    writer.writerow([line.get('id')])
-
-       #writer.writerow([line.get(["element_texts"][0]["text"])])
-    
-
-#    writer.writerow(["element_texts"][0]["text"])
-
-    #    writer.writerow([line.get('element_texts').get('element')])
-    #writer.writerow([line.get('element_texts').get('GATE Reference code'),
-     #                line.get('element_texts').get('GATE Title'),
-      #               line.get('element_texts').get('GATE Name of Creator'),
-       #              line.get('element_texts').get('GATE Dates of Creation')])
-
-
 
 csv_out.close()
